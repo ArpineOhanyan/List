9d46f73b98afd571574d3ba186e109f549a191e1
ArpineOhanyan <arpineohanyan3@gmail.com>
2021-06-20 09:41:40 +0400

    correct iterator class

--- node.cpp
+++ node.cpp
@@ -1,22 +1,5 @@
 #include "node.hpp"
 
-/*
-class Node
-{
-    public:
-        int data;
-        Node* prev;
-        Node* next;
-    public:
-        Node();
-        Node(const int& value)
-            :data(value),prev(nullptr),next(nullptr){}
-        Node(const Node& node) {
-            this->data = node.data;
-            prev = node.prev;
-            next = node.next;
-        }
-};*/
 Node::Node()
 	:data(0),
 	prev(nullptr),
